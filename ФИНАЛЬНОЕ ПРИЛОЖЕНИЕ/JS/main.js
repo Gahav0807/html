9287cba0ff8afb4693e4aff587e4a216e5f2801f
Gahav0807 <143396539+Gahav0807@users.noreply.github.com>
2023-08-28 12:38:54 +0300

    добавили в проект наш файл html,css,js для github

---
+++ ФИНАЛЬНОЕ ПРИЛОЖЕНИЕ/JS/main.js
@@ -0,0 +1,21 @@
+function redirectToOtherSite() {
+    window.location.href = 'file:///C:/Users/79884/Desktop/%D0%93%D0%BE%D0%B4%D0%BE%D0%B2%D0%BE%D0%B9%20%D0%BF%D1%80%D0%BE%D0%B5%D0%BA%D1%82%208%20%D0%BA%D0%BB%D0%B0%D1%81%D1%81%20%D0%94%D1%83%D0%BC%D0%B0%D0%BD%D1%8F%D0%BD%20%D0%92%D0%B0%D0%B0%D0%B3%D0%BD/%D0%A7%D0%B0%D1%81%D1%8B/index.html'
+
+}
+
+function redirectToOtherSite1() {
+    window.location.href = 'file:///C:/Users/79884/Desktop/%D0%93%D0%BE%D0%B4%D0%BE%D0%B2%D0%BE%D0%B9%20%D0%BF%D1%80%D0%BE%D0%B5%D0%BA%D1%82%208%20%D0%BA%D0%BB%D0%B0%D1%81%D1%81%20%D0%94%D1%83%D0%BC%D0%B0%D0%BD%D1%8F%D0%BD%20%D0%92%D0%B0%D0%B0%D0%B3%D0%BD/%D0%9F%D0%BB%D0%B0%D0%BD%D0%B8%D1%80%D0%BE%D0%B2%D1%89%D0%B8%D0%BA%20%D1%81%20%D0%BD%D0%B0%D0%BF%D0%BE%D0%BC%D0%B8%D0%BD%D0%B0%D0%BD%D0%B8%D0%B5%D0%BC/main.html'
+
+
+}
+
+function redirectToOtherSite2() {
+    window.location.href = 'file:///C:/Users/79884/Desktop/%D0%93%D0%BE%D0%B4%D0%BE%D0%B2%D0%BE%D0%B9%20%D0%BF%D1%80%D0%BE%D0%B5%D0%BA%D1%82%208%20%D0%BA%D0%BB%D0%B0%D1%81%D1%81%20%D0%94%D1%83%D0%BC%D0%B0%D0%BD%D1%8F%D0%BD%20%D0%92%D0%B0%D0%B0%D0%B3%D0%BD/Planer/main.html'
+
+}
+
+function redirectToOtherSite3() {
+    window.location.href = 'file:///C:/Users/79884/Desktop/%D0%93%D0%BE%D0%B4%D0%BE%D0%B2%D0%BE%D0%B9%20%D0%BF%D1%80%D0%BE%D0%B5%D0%BA%D1%82%208%20%D0%BA%D0%BB%D0%B0%D1%81%D1%81%20%D0%94%D1%83%D0%BC%D0%B0%D0%BD%D1%8F%D0%BD%20%D0%92%D0%B0%D0%B0%D0%B3%D0%BD/Notes%202/index.html'
+
+}
+
